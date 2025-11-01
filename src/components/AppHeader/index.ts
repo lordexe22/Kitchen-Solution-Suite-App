c2551f84e6480ec12233a6b77f6b2b895234628a
lordexe22 <walterezequielpuig@gmail.com>
2025-11-01 14:21:11 -0300

    remove empty files

--- src/components/AppHeader/index.ts
+++ src/components/AppHeader/index.ts
@@ -1,4 +1,4 @@
 // src/components/AppHeader/index.ts
 export { default } from './AppHeader';
 export { default as AppHeader } from './AppHeader';
-export type { AppHeaderProps, AppHeaderUser } from './AppHeader.types';
+export type { AppHeaderProps } from './AppHeader.types';
